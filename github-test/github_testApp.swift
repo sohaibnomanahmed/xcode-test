31eb4167416ad77745b8dd47dd6b68b89d21923c
Sohaib Ahmed <sohaib.noman.ahmed@gmail.com>
2022-08-12 06:22:26 +0200

    Initial Commit

---
+++ github-test/github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_testApp.swift
+//  github-test
+//
+//  Created by Sohaib Ahmed on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
